b5299e3c07575385191e0817855fb8f367b37967
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-04 00:10:23 +0530

    StarRating created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Accordian from './components/accordion';
+import StarRating from './components/starRating';
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
      <Accordian/>
+     {/* Star rating */}
+     <StarRating noOfStars= {10} />
     </div>
   );
 }
