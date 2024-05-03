478475e6f20129a7ae945ff36f64ba7ee071904c
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-04 00:11:11 +0530

    final commit done

--- src/components/starRating/index.jsx
+++ src/components/starRating/index.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
-import './style.css';
+import "./style.css";
 
-export default function StarRating({noOfStars = 10}) {
+export default function StarRating({ noOfStars = 10 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   function handleClick(getCurrentIndex) {
-    setRating(getCurrentIndex)
+    setRating(getCurrentIndex);
   }
   function handleMouseEnter(getCurrentIndex) {
-    setHover(getCurrentIndex)
+    setHover(getCurrentIndex);
   }
   function handleMouseLeave() {
-    setHover(rating)
-    }
+    setHover(rating);
+  }
   return (
     <div className="star-rating">
       {[...Array(noOfStars)].map((_, index) => {
