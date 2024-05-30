e4dca5f7f26a5d50a0a80878ead47da2ead744df
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-30 18:31:05 +0530

    final commit

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,44 +1,40 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-export default function ImageSlider({url, limit = 5, page = 1}) {
-    const [images, setImages] = useState([])
-    const [currentSlide, setCurrentSlide] = useState(0)
-    const [errorMsg, setErrorMsg] = useState(null)
-    const [loading, setLoading] = useState(false)
-
-    async function fetchImages(getUrl){
-        try{
-            setLoading(true)
-            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`)
-            const data = await response.json()
-
-            if(data){
-                setImages(data)
-                setLoading(false)
-            }
-        }catch(e){
-            setErrorMsg(e.message)
-            setLoading(false)
-        }
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
     }
+  }
 
-    useEffect(() => {
-        if(url != '') fetchImages()
-    }, [url])
+  useEffect(() => {
+    if (url != "") fetchImages();
+  }, [url]);
 
-    console.log(images);
+  console.log(images);
 
-    if(loading) {
-        return <div>Loading data! PLease wait</div>
-    }
-    if(errorMsg !== null) {
-        return <div>Error occured ! {errorMsg}</div>
-    }
+  if (loading) {
+    return <div>Loading data! PLease wait</div>;
+  }
+  if (errorMsg !== null) {
+    return <div>Error occured ! {errorMsg}</div>;
+  }
 
-
-
-    return <div className='container'></div>
+  return <div className="container"></div>;
 }
