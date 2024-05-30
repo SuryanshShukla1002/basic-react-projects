d4f141b1969f1e880c7384eedd9f06421cb25f74
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-30 18:30:31 +0530

    useffect bug fix

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -3,18 +3,40 @@ import { useEffect } from 'react';
 
 
 
-export default function ImageSlider({url, limit}) {
+export default function ImageSlider({url, limit = 5, page = 1}) {
     const [images, setImages] = useState([])
     const [currentSlide, setCurrentSlide] = useState(0)
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     async function fetchImages(getUrl){
-        
+        try{
+            setLoading(true)
+            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`)
+            const data = await response.json()
+
+            if(data){
+                setImages(data)
+                setLoading(false)
+            }
+        }catch(e){
+            setErrorMsg(e.message)
+            setLoading(false)
+        }
     }
 
     useEffect(() => {
         if(url != '') fetchImages()
     }, [url])
 
+    console.log(images);
+
+    if(loading) {
+        return <div>Loading data! PLease wait</div>
+    }
+    if(errorMsg !== null) {
+        return <div>Error occured ! {errorMsg}</div>
+    }
 
 
 
