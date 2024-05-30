e03a24ea8a1fef5b9c713f42a7d7478e33f1a322
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-30 18:30:11 +0530

    Image Slider page limit set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordian from './components/accordion';
+import ImageSlider from './components/image-slider';
 import StarRating from './components/starRating';
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
      <Accordian/>
      {/* Star rating */}
      <StarRating noOfStars= {10} />
+     {/* IMage slider  */}
+     <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
     </div>
   );
 }
