90988cb178dc8768bb40a89d82c21de741c1d0c0
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-06-04 00:16:10 +0530

    product title shown

--- src/components/load-more-button/index.jsx
+++ src/components/load-more-button/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import './styles.css';
 
 export default function LoadMoreData() {
   const [loading, setLoading] = useState(false);
@@ -7,21 +8,46 @@ export default function LoadMoreData() {
   const [count, setCount] = useState(0);
   async function fetchProducts() {
     try {
+        setLoading(true)
       const response = await fetch(
         `https://dummyjson.com/products?limit=20&skip=${
           count === 0 ? 0 : count * 20
         }`
       );
       const result = await response.json()
-      
+      if(result  && result.products && result.products.length) {
+        setProducts(result.products)
+        setLoading(false)
+      }
+
       console.log(result);
     } catch (e) {
       console.log(e);
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     fetchProducts();
   }, []);
-  return <div className="container"></div>;
+
+  if(loading){
+    return <div>Loading Data! please wait</div>
+  }
+  return (
+  <div className="load-more-container">
+    <div className='product-container'>
+        {
+            products && products.length ? products.map(item => <div key={item.id} className='product'>
+                <img src={item.thumbnail} alt={item.title}/>
+                <p>{item.title}</p>
+            </div>) : null
+        }
+    </div>
+    <div>
+        <button className='button-container'>LOad more products</button>
+    </div>
+  </div>
+
+)
 }
